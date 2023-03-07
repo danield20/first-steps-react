64a325b739920b147b860e1bee0ccdf6c0e8736d
Daniel Dinca <dincadaniel97@gmail.com>
2023-03-07 22:44:14 +0200

    authentification

---
+++ Authentification/frontend/src/pages/Root.js
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
+
+import MainNavigation from "../components/MainNavigation";
+import { getTokenDuration } from "../util/auth";
+
+function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+
+    const toeknDuration = getTokenDuration();
+    console.log(toeknDuration);
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, toeknDuration);
+  }, [token, submit]);
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
